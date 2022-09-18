5656e6a1655f3c03520e712ac954a6a507160f2c
Kota <maedakoutaa@gmail.com>
2022-09-18 21:33:41 +0900

    commit

---
+++ app/src/main/java/com/example/miniandroidapp01_helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.miniandroidapp01_helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+        Button buttonView = findViewById(R.id.button);
+
+    }
+}
